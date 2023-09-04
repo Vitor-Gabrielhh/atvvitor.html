22cedb102f73e2ac4945bc4284b31fcb01e3738c
Vitor-Gabrielhh <143191366+Vitor-Gabrielhh@users.noreply.github.com>
2023-09-04 10:51:57 -0400

    Add files via upload

---
+++ index2.js
@@ -0,0 +1,40 @@
+const ordem = {
+    alfabetica: ['Espanha', 'Belgica', 'Alemanha', 'Dourados', 'Camisão'],
+    alfa: function () {
+        this.alfabetica.sort();
+        console.log(this.alfabetica);
+    }
+};
+ordem.alfa();
+
+const remov = {
+    numeros: [3, 7, 0, 5, 9],
+    remover: function () {
+        this.numeros.pop();
+
+        console.log(this.numeros);
+    }
+};
+
+remov.remover();
+   
+const array = {
+    numerosUm: [8, 5, 2, 6, 0],
+    numerosDois: [4, 2, 9, 9,],
+    metodo: function () {
+        this.numerosUm.push(...this.numerosDois);
+        this.numerosUm.sort();
+        this.numerosDois.sort();
+        console.log(this.numerosUm);
+    }
+};
+array.metodo()
+
+const desordem = {
+    bagunca: ['Alemanha','Belgica' ,'Camisão','Dourados','Espanha'],
+    desordemAlfa: function () {
+        this.bagunca.reverse();
+        console.log(this.bagunca);
+    }
+};
+desordem.desordemAlfa();
